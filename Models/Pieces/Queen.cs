9cc235e5d20eb4f9c11e1ff02b010edc17e17d61
112axel <axel.cullbrand@gmail.com>
2023-05-28 21:44:06 +0200

    Added queen and rook

---
+++ Models/Pieces/Queen.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorChess.Models.Pieces
+{
+    public class Queen : Piece
+    {
+        public Queen(bool isBlack) : base("queen", isBlack)
+        {
+        }
+
+        public override List<Move> AllowedMoves(Board board, int x, int y)
+        {
+
+            var allAlowedMoves = DiagonalMove(board, x, y); 
+            allAlowedMoves.AddRange(LinearMove(board,x,y));
+            return allAlowedMoves;
+        }
+    }
+}
